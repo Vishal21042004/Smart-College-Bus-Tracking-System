6d18202cf345390fb3fab39b1e9b9e94847f909d
Vishal Kadalagi <121157713+Vishal21042004@users.noreply.github.com>
2025-03-21 09:24:16 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter_windows_3.27.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\mini\\campus_go121" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter_windows_3.27.1-stable\\flutter"
+  "PROJECT_DIR=D:\\mini\\campus_go121"
+  "FLUTTER_ROOT=D:\\flutter_windows_3.27.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\mini\\campus_go121\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\mini\\campus_go121"
+  "FLUTTER_TARGET=D:\\mini\\campus_go121\\lib/main_student.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\mini\\campus_go121\\.dart_tool\\package_config.json"
+)
